c9d7f1d0a5e8b6f024916d733d08006381ca04ea
Reis-Silva J.C <cesar.everlastingtechnologydeveloper@outlook.com>
2020-10-26 16:46:02 -0300

    Ajustando dados de cotação de moeda

---
+++ PrototipoViabilidadeCotacao/src/main/java/br/com/cotacao/entidade/datasource/Moedas.java
@@ -0,0 +1,18 @@
+package br.com.cotacao.entidade.datasource;
+
+import java.util.Date;
+
+public class Moedas {
+	
+	private int id;
+	private String moedaOrigem;
+	private String moedaDestino;
+	private double vrlCompra;
+	private double vlrVenda;
+	private double percentualLuvro = 2.5;
+	private double vrlCompraAjuste;
+	private double vrlVendaAjuste;
+	private Date datetime;
+	
+
+}
